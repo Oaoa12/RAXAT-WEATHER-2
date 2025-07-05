6542683aa6d5ced16176f7a8acc1932a02a10c73
Oaoa12 <ishikhakhmedoa@mail.ru>
2025-07-05 15:29:36 +0300

    sefes

--- src/components/pages/favoritesPage/WeatherEffect.jsx
+++ src/components/pages/favoritesPage/WeatherEffect.jsx
@@ -1,70 +1,61 @@
-import React from 'react'
-import './WeatherEffect.scss'
+import React from 'react';
+import './WeatherEffect.scss';
 
 const WeatherEffect = ({ data }) => {
-
-  const ShowEffect = data?.weather?.[0]?.main
-
+  const ShowEffect = data?.weather?.[0]?.main;
 
   return (
     <>
-  <div className="rain-container">
-    {ShowEffect === 'Rain' &&
-      [...Array(100)].map((_, i) => (
-        <div 
-          key={i}
-          className="rain-drop"
-          style={{
-            left: `${Math.random() * 100}%`,
-            animationDuration: `${0.5 + Math.random() * 0.5}s`,
-            animationDelay: `${Math.random() * 2}s`,
-            opacity: Math.random(),
-          }}
-        />
-      ))
-    }
-  </div>
-  
-  {ShowEffect === 'Clear' && (
-        <div className="sun-glow"></div>
-      )}
-
-
-      {ShowEffect === 'Snow' && (
-        <div className="snow-container">
-        {[...Array(100)].map((_, i) => {
-          const size = Math.random() * 0.5 + 0.2; 
-          const duration = 10 + Math.random() * 20; 
-          const delay =  0; 
-          const left = Math.random() * 100; 
-          
-          return (
-            <div
+      {ShowEffect === 'Rain' && (
+        <div className="rain-container">
+          {[...Array(100)].map((_, i) => (
+            <div 
               key={i}
-              className="snowflake"
+              className="rain-drop"
               style={{
-                left: `${left}%`,
-                width: `${size}rem`,
-                height: `${size}rem`,
-                animationDuration: `${duration}s`,
-                animationDelay: `${delay}s`,
-                opacity: Math.random() * 0.7 + 0.3, 
+                left: `${Math.random() * 100}%`,
+                top: `${Math.random() * -100}px`, 
+                animationDuration: `${0.5 + Math.random() * 0.5}s`,
+                opacity: Math.random(),
               }}
             />
-          );
-        })}
-      </div>
+          ))}
+        </div>
       )}
 
+      {ShowEffect === 'Clear' && <div className="sun-glow"></div>}
+      
+      {ShowEffect === 'Snow' && (
+        <div className="snow-container">
+          {[...Array(100)].map((_, i) => {
+            const size = Math.random() * 0.5 + 0.2;
+            const duration = 10 + Math.random() * 20;
+            const left = Math.random() * 100;
+            
+            return (
+              <div
+                key={i}
+                className="snowflake"
+                style={{
+                  left: `${left}%`,
+                  width: `${size}rem`,
+                  height: `${size}rem`,
+                  animationDuration: `${duration}s`,
+                  opacity: Math.random() * 0.7 + 0.3,
+                }}
+              />
+            );
+          })}
+        </div>
+      )}
 
       {ShowEffect === 'Clouds' && (
-          <div className="subtle-fog">
+        <div className="subtle-fog">
           <div className="fog-veil"></div>
         </div>
       )}
-</>
+    </>
+  );
+};
 
-  )
-}
-
-export default WeatherEffect
+export default WeatherEffect;
